8c231a475fc978523bdba869485ebfe84884fad4
ppoliani <ppoliani@gmail.com>
2022-07-24 17:29:39 +0300

    feat: add api helpers crate

---
+++ src/api-helpers/src/utils/store.rs
@@ -0,0 +1,15 @@
+use fireauth::FireAuth;
+
+pub struct Store {
+  pub firebase_auth: FireAuth,
+}
+
+impl Store {
+  pub async fn new(firebase_auth_key: String) -> Self {
+    let firebase_auth = fireauth::FireAuth::new(firebase_auth_key.clone());
+
+    Self {
+      firebase_auth,
+    }
+  }
+}

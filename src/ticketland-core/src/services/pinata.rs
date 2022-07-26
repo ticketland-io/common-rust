5068e402585c4211673b2e528a0336d3172eb831
ppoliani <ppoliani@gmail.com>
2022-07-26 19:11:26 +0300

    feat: use multipart in the pinata upload method

--- src/ticketland-core/src/services/pinata.rs
+++ src/ticketland-core/src/services/pinata.rs
@@ -1,4 +1,8 @@
-use reqwest::{Client, Body};
+use reqwest::{
+  Client,
+  Body,
+  multipart::{Form, Part},
+};
 use tokio::io::AsyncRead;
 use tokio_util::io::ReaderStream;
 use crate::error::Error;
@@ -30,10 +34,15 @@ impl Pinata {
   where 
     R: AsyncRead + Send + Sync + 'static
   {
+    let form = Form::new()
+    .part("file", Part::stream(Body::wrap_stream(data_stream)))
+    .text("pinataOptions", "{\"cidVersion\": 1}")
+    .text("pinataMetadata", "{\"name\": \"MyFile\", \"keyvalues\": {\"company\": \"Pinata\"}}");
+
     self.pinata_client.post(format!("{}/pinning/pinFileToIPFS", self.pinata_api_url))
     .header("pinata_api_key", self.pinata_api_key.clone())
     .header("pinata_secret_api_key", self.pinata_api_secret.clone())
-    .body(Body::wrap_stream(data_stream))
+    .multipart(form)
     .send()
     .await
     .map_err(Into::<Error>::into)?;

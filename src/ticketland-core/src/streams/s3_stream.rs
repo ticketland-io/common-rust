4218eb2984b56380197a45e7720ee5f1e382514d
ppoliani <ppoliani@gmail.com>
2022-07-30 17:30:07 +0300

    fix: should impl Sync

--- src/ticketland-core/src/streams/s3_stream.rs
+++ src/ticketland-core/src/streams/s3_stream.rs
@@ -26,7 +26,7 @@ pub struct S3Stream {
 }
 
 unsafe impl Send for S3Stream {}
-unsafe impl Send for S3Stream {}
+unsafe impl Sync for S3Stream {}
 
 /// The `max_buf_size` argument is the maximum amount of bytes that can be
 /// written to a side before the write returns `Poll::Pending`.

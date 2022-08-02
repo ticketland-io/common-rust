20713cf7428ba2b42c537bba11b243217f42c21a
ppoliani <ppoliani@gmail.com>
2022-08-02 16:14:07 +0300

    feat: add props to the Event model

--- src/common-data/src/models/event.rs
+++ src/common-data/src/models/event.rs
@@ -9,9 +9,11 @@ use crate::types::Neo4jResult;
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct Event {
-  // TODO: Add additional fields
-  pub title: String,
-  pub description: String,
+  pub event_id: String,
+  pub image_type: String,
+  pub metadata_cid: String,
+  pub metadata_uploaded: bool,
+  pub image_uploaded: bool,
 }
 
 impl TryFrom<Neo4jResult> for Event {
@@ -24,30 +26,39 @@ impl TryFrom<Neo4jResult> for Event {
 
     let value = v.0.get(0).unwrap().clone();
 
-    let account = match value {
+    let event = match value {
       Value::Map(_) => {
         let map = HashMap::<String, Value>::try_from(value).expect("cannot convert value to map");
-        let mut account = Event {
+        let mut event = Event {
           ..Default::default()
         };
 
         for (k, v) in map {
           match k.as_str() {
             "title" => {
-              account.title = String::try_from(v).expect("cannot convert value to staking_token");
+              event.event_id = String::try_from(v).expect("cannot convert title");
             },
-            "description" => {
-              account.description = String::try_from(v).expect("cannot convert value to staking_token");
+            "image_type" => {
+              event.image_type = String::try_from(v).expect("cannot convert image_type");
+            },
+            "metadata_cid" => {
+              event.metadata_cid = String::try_from(v).expect("cannot convert metadata_cid");
+            },
+            "metadata_uploaded" => {
+              event.metadata_uploaded = bool::try_from(v).expect("cannot convert metadata_uploaded");
+            },
+            "image_uploaded" => {
+              event.image_uploaded = bool::try_from(v).expect("cannot convert image_uploaded");
             },
             _ => panic!("unknown field"),
           }
         }
 
-        account
+        event
       }
       _ => panic!("neo4j result should be a Value::Map"),
     };
 
-    Ok(account)
+    Ok(event)
   }
 }

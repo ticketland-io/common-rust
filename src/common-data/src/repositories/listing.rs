df1b3e19428629bf0cc2065e3be22f5b6313dc30
ppoliani <ppoliani@gmail.com>
2022-08-25 12:26:45 +0300

    feat(data): implement cancel_sell_listing

--- src/common-data/src/repositories/listing.rs
+++ src/common-data/src/repositories/listing.rs
@@ -15,7 +15,7 @@ pub fn create_sell_listing(
     MATCH (acc:Account {uid: $uid})
     MATCH (t:Ticket {ticket_metadata:$ticket_metadata})
     MERGE (acc)-[:HAS_SELL_LISTING {open: true}]->(sl:SellListing {
-      sell_listing_account:sell_listing_account,
+      account:sell_listing_account,
       ask_price:$ask_price,
       created_at:$created_at
     })-[:FOR]->(t)
@@ -44,7 +44,7 @@ pub fn create_buy_listing(
     MATCH (evt:Event {event_id:$event_id})
     MATCH (acc:Account {uid: $uid})
     MERGE (acc)-[:HAS_BUY_LISTING]->(bl:BuyListing {
-      buy_listing_account:$buy_listing_account,
+      account:$buy_listing_account,
       bid_price:$bid_price,
       created_at:$created_at
     })-[:FOR_TICKET_OF]->(evt)
@@ -72,7 +72,7 @@ pub fn fill_sell_listing(
 ) -> (&'static str, Option<Params>) {
   let query = r#"
     MATCH (ticket_buyer:Account {uid: $ticket_buyer_uid})
-    MATCH (ticket_buyer)-[hsl:HAS_SELL_LISTING {open: true}]->(sl:SellListing {sell_listing_account:$sell_listing_account})
+    MATCH (ticket_buyer)-[hsl:HAS_SELL_LISTING {open: true}]->(sl:SellListing {account:$sell_listing_account})
     MATCH (:Account)-[ht:HAS_TICKET {owner: true}]->(t:Ticket {ticket_metadata:$ticket_metadata})
     SET hsl.open = false
     SET ht.owner = false
@@ -98,7 +98,7 @@ pub fn fill_buy_listing(
 ) -> (&'static str, Option<Params>) {
   let query = r#"
     MATCH (ticket_seller:Account {uid: $ticket_seller_uid})-[ht:HAS_TICKET {owner: true}]->(t:Ticket {ticket_metadata:$ticket_metadata})
-    MATCH (ticket_buyer:Account)-[hbl:HAS_BUY_LISTING {open: true}]->(bl:BuyListing {buy_listing_account:$buy_listing_account})
+    MATCH (ticket_buyer:Account)-[hbl:HAS_BUY_LISTING {open: true}]->(bl:BuyListing {account:$buy_listing_account})
     SET hbl.open = false
     SET ht.owner = false
     CREATE (ticket_buyer)-[ht:HAS_TICKET {owner: true, created_at:$created_at}]->(t)
@@ -110,6 +110,25 @@ pub fn fill_buy_listing(
     ("buy_listing_account", Value::String(buy_listing_account)),
     ("ticket_metadata", Value::String(ticket_metadata)),
     ("created_at", Value::Integer(created_at.into())),
+  ]);
+
+  (query, params)
+}
+
+pub fn cancel_sell_listing(
+  ticket_seller_uid: String,
+  sell_listing_account: String,
+) -> (&'static str, Option<Params>) {
+  let query = r#"
+    MATCH (ticket_seller:Account {uid: $ticket_seller_uid})
+    MATCH (ticket_seller)-[hsl:HAS_SELL_LISTING {open: true}]->(sl:SellListing {account:$sell_listing_account})
+    SET hsl.open = false
+    RETURN 1
+  "#;
+
+  let params = create_params(vec![
+    ("ticket_seller_uid", Value::String(ticket_seller_uid)),
+    ("sell_listing_account", Value::String(sell_listing_account)),
   ]);
 
   (query, params)

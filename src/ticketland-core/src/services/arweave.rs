fd380aba356ba044aff5215382d14e9a96a6f8a9
ppoliani <ppoliani@gmail.com>
2022-09-08 15:25:29 +0300

    feat(core): implement the new fn of the arweave client

---
+++ src/ticketland-core/src/services/arweave.rs
@@ -0,0 +1,40 @@
+use std::str::FromStr;
+use arloader::{
+  Arweave,
+  commands::*,
+  error::Error,
+  status::OutputFormat,
+  crypto::Provider,
+};
+use ring::{
+  rand,
+  signature,
+};
+use jsonwebkey::JsonWebKey;
+use url::Url;
+
+pub struct Client {
+  arweave: Arweave,
+}
+
+impl Client {
+  pub async fn new(key_pair: String) -> Result<Self, Error> {
+    let jwk_parsed: JsonWebKey = key_pair.parse().unwrap();
+    let crypto = Provider {
+      keypair: signature::RsaKeyPair::from_pkcs8(&jwk_parsed.key.as_ref().to_der())?,
+      sr: rand::SystemRandom::new(),
+    };
+
+    let arweave = Arweave {
+        base_url: Url::from_str("https://arweave.net").unwrap(),
+        crypto,
+        ..Default::default()
+    };
+
+    Ok(
+      Self {
+        arweave,
+      }
+    )
+  }
+}
--- src/ticketland-core/src/services/mod.rs
+++ src/ticketland-core/src/services/mod.rs
@@ -1,3 +1,4 @@
 pub mod minio;
 pub mod ipfs;
 pub mod pinata;
+pub mod arweave;

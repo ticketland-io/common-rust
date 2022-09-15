a47fc04f86c5aae4590ef2fa62d09f1cf5310d2d
ppoliani <ppoliani@gmail.com>
2022-09-15 15:55:49 +0300

    feat(core): implement upload_file_as_bundle

--- src/ticketland-core/src/services/arweave.rs
+++ src/ticketland-core/src/services/arweave.rs
@@ -5,6 +5,7 @@ use std::{
 use arloader::{
   upload_files_stream,
   Arweave,
+  bundle::DataItem,
   status::Status,
   error::Error,
   crypto::Provider,
@@ -53,6 +54,48 @@ impl Client {
     })
   }
 
+  fn create_tags_str(tags: Option<Vec<(&str, &str)>>) -> Option<Vec<Tag<String>>>{
+    tags
+    .map(|tags| {
+      tags
+      .into_iter()
+      .map(|(name, value)| Tag::<String>::from_utf8_strs(name, value).expect("valid tag"))
+      .collect()
+    })
+  }
+
+
+  fn get_content_type(data: &Vec<u8>,) -> String {
+    if let Some(kind) = infer::get(&data) {
+      kind.mime_type().to_owned()
+    } else {
+        "application/octet-stream".to_owned()
+    }
+  }
+
+  pub fn create_data_item(
+    &self,
+    data: Vec<u8>,
+    other_tags: Option<Vec<(&str, &str)>>,
+    auto_content_tag: bool,
+  ) -> Result<(DataItem, Status), Error> {
+    let content_type = Self::get_content_type(&data);
+    let data_item = self.arweave.create_data_item(
+      data,
+      Self::create_tags_str(other_tags).unwrap_or(vec![]),
+      auto_content_tag,
+    )?;
+    let data_item = self.arweave.sign_data_item(data_item)?;
+
+    let status = Status {
+      id: data_item.id.clone(),
+      content_type,
+      ..Status::default()
+    };
+
+    Ok((data_item, status))
+  }
+
   /// Upload the given binary data to Arweave. This does not handle bundles and the max
   /// size should be MAX_TX_DATA 10mb.
   /// 
@@ -84,6 +127,37 @@ impl Client {
     ).await?;
 
     let signed_tx = self.arweave.sign_transaction(tx)?;
+    self.arweave.post_transaction(&signed_tx).await
+  }
+
+  pub async fn upload_file_as_bundle(
+    &self,
+    data: Vec<u8>,
+    other_tags: Option<Vec<(&str, &str)>>,
+    reward_mult: f32,
+    auto_content_tag: bool,
+  ) -> Result<(Base64, u64), Error> {
+    let data_item = self.create_data_item(data, other_tags.clone(), auto_content_tag)?;
+
+    let (bundle, _) = self.arweave.create_bundle_from_data_items(vec![data_item])?;
+    let other_tags = other_tags.map(|mut tags| {
+      tags.push(("Bundle-Format", "binary"));
+      tags.push(("Bundle-Version", "2.0.0"));
+
+      tags
+    });
+    
+    let price_terms = self.arweave.get_price_terms(reward_mult).await?;
+    let tx = self.arweave.create_transaction(
+      bundle,
+      Self::create_tags(other_tags),
+      None,
+      price_terms,
+      true,
+    )
+    .await?;
+    let signed_tx = self.arweave.sign_transaction(tx)?;
+
     self.arweave.post_transaction(&signed_tx).await
   }
 

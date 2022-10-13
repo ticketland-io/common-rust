a778bedab91759c1437bda2e8fd05f64ffb397cd
ppoliani <ppoliani@gmail.com>
2022-10-13 22:13:56 +0300

    feat: add stripe_uid to upsert_account_link

--- src/common-data/src/repositories/stripe.rs
+++ src/common-data/src/repositories/stripe.rs
@@ -4,22 +4,6 @@ use ticketland_core::{
   actor::neo4j::{create_params}
 };
 
-
-pub fn create_stripe_user(uid: String, stripe_uid: String) -> (&'static str, Option<Params>) {
-  let query = r#"
-    MATCH (acc:Account {uid: $uid})
-    MERGE (acc)-[:IS_STRIPE_USER]->(su:StripeUser {stripe_uid: $stripe_uid})
-    RETURN su{.*}
-  "#;
-
-  let params = create_params(vec![
-    ("uid", Value::String(uid)),
-    ("stripe_uid", Value::String(stripe_uid)),
-  ]);
-
-  (query, params)
-}
-
 pub fn read_stripe_user(uid: String) -> (&'static str, Option<Params>) {
   let query = r#"
     MATCH (acc:Account {uid: $uid})-[:IS_STRIPE_USER]->(su:StripeUser)
@@ -33,10 +17,10 @@ pub fn read_stripe_user(uid: String) -> (&'static str, Option<Params>) {
   (query, params)
 }
 
-pub fn upsert_account_link(uid: String, account_link: String) -> (&'static str, Option<Params>) {
+pub fn upsert_account_link(uid: String, stripe_uid: String, account_link: String) -> (&'static str, Option<Params>) {
   let query = r#"
     MATCH (acc:Account {uid: $uid})
-    MERGE (acc)-[:IS_STRIPE_USER]->(su:StripeUser)
+    MERGE (acc)-[:IS_STRIPE_USER]->(su:StripeUser {stripe_uid: $stripe_uid})
     ON CREATE SET su.account_link = $account_link
     ON MATCH SET su.account_link = $account_link
     RETURN su{.*}
@@ -44,6 +28,7 @@ pub fn upsert_account_link(uid: String, account_link: String) -> (&'static str, 
 
   let params = create_params(vec![
     ("uid", Value::String(uid)),
+    ("stripe_uid", Value::String(stripe_uid)),
     ("account_link", Value::String(account_link)),
   ]);
 

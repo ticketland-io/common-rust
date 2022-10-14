94f1cd6a0044e2f0bc576721924f6ac24a742394
ppoliani <ppoliani@gmail.com>
2022-10-14 10:28:58 +0300

    feat(data): implement update_stripe_account_status

--- src/common-data/src/repositories/stripe.rs
+++ src/common-data/src/repositories/stripe.rs
@@ -20,7 +20,7 @@ pub fn read_stripe_user(uid: String) -> (&'static str, Option<Params>) {
 pub fn upsert_account_link(uid: String, stripe_uid: String, account_link: String) -> (&'static str, Option<Params>) {
   let query = r#"
     MATCH (acc:Account {uid: $uid})
-    MERGE (acc)-[:IS_STRIPE_USER]->(su:StripeUser {stripe_uid: $stripe_uid})
+    MERGE (acc)-[:IS_STRIPE_USER]->(su:StripeUser {stripe_uid: $stripe_uid, status: 0})
     ON CREATE SET su.account_link = $account_link
     ON MATCH SET su.account_link = $account_link
     RETURN su{.*}
@@ -30,6 +30,20 @@ pub fn upsert_account_link(uid: String, stripe_uid: String, account_link: String
     ("uid", Value::String(uid)),
     ("stripe_uid", Value::String(stripe_uid)),
     ("account_link", Value::String(account_link)),
+  ]);
+
+  (query, params)
+}
+
+pub fn update_stripe_account_status(stripe_uid: String) -> (&'static str, Option<Params>) {
+  let query = r#"
+    MATCH (su:StripeUser {stripe_uid: $stripe_uid})
+    SET su.status = 1
+    RETURN 1
+  "#;
+
+  let params = create_params(vec![
+    ("stripe_uid", Value::String(stripe_uid)),
   ]);
 
   (query, params)

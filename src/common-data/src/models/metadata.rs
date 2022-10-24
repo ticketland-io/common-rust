a49d5c1509695b3f1489e8c25d4902468affd151
George Niotis <geoniotis@yahoo.com>
2022-10-24 11:19:13 +0300

    feat: derive clone on metadata and attribute model struct

--- src/common-data/src/models/metadata.rs
+++ src/common-data/src/models/metadata.rs
@@ -1,12 +1,12 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Attribute {
   pub trait_type: String,
   pub value: String,
 }
 
-#[derive(Default, Debug, Deserialize, Serialize)]
+#[derive(Default, Debug, Deserialize, Serialize, Clone)]
 pub struct Metadata {
   pub name: String,
   pub description: String,

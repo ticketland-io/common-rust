34d6b113c5362a0d92bc4dc84fd482bf374887d4
ppoliani <ppoliani@gmail.com>
2022-10-31 18:26:57 +0200

    feat(crypto): implement sign

--- src/crypto/src/ec/ed25519.rs
+++ src/crypto/src/ec/ed25519.rs
@@ -4,12 +4,18 @@ use ed25519_dalek::{
   Keypair,
   PublicKey,
   Signature,
+  Signer,
   Verifier,
 };
 
 pub fn create_keypair() -> Keypair {
   let mut csprng = OsRng{};
   Keypair::generate(&mut csprng)
+}
+
+pub fn sign(msg: &[u8], keypair: &[u8]) -> Result<Signature> {
+  let keypair: Keypair = serde_json::from_slice(&keypair)?;
+  Ok(keypair.sign(msg))
 }
 
 pub fn verify(msg: &[u8], pub_key: &[u8], signature: Signature) -> Result<()> {

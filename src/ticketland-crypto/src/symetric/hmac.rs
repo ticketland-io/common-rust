70603bdaed171e657a1d2a1bb300f9ae97a9881d
ppoliani <ppoliani@gmail.com>
2022-10-31 21:18:24 +0200

    feat(cryptoadd the old hmac back for testing):

--- src/ticketland-crypto/src/symetric/hmac.rs
+++ src/ticketland-crypto/src/symetric/hmac.rs
@@ -1,21 +1,32 @@
 use eyre::Result;
-use ring::{
-  digest,
-  hmac,
-  rand
-};
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
+// use ring::{
+//   digest,
+//   hmac,
+//   rand
+// };
 
 pub fn sign_sha256(secret_key: &str, msg: &str) -> Result<String> {
   let key = base64::decode(secret_key)?;
-  let s_key = hmac::Key::new(hmac::HMAC_SHA256, key.as_ref());
-  let tag = hmac::sign(&s_key, msg.as_bytes());
+  let s_key = ring::hmac::Key::new(ring::hmac::HMAC_SHA256, key.as_ref());
+  let tag = ring::hmac::sign(&s_key, msg.as_bytes());
 
   Ok(hex::encode(&tag.as_ref()))
 }
 
+pub fn sign_sha256_2(secret_key: &str, message: &str) -> Result<String> {
+  let key = base64::decode(secret_key)?;
+  let mut mac = Hmac::<Sha256>::new_from_slice(key.as_ref()).unwrap();
+
+  mac.update(format!("{}", message).as_bytes());
+  Ok(hex::encode(&mac.finalize().into_bytes()[..]))
+}
+
+
 pub fn create_key() -> Result<String>{
-  let rng = rand::SystemRandom::new();
-  let key_value: [u8; digest::SHA256_OUTPUT_LEN] = rand::generate(&rng)?.expose();
+  let rng = ring::rand::SystemRandom::new();
+  let key_value: [u8; ring::digest::SHA256_OUTPUT_LEN] = ring::rand::generate(&rng)?.expose();
 
   Ok(hex::encode(&key_value))
 }

a116e6b35c84e30c9690a224eb350a48eaf129f9
ppoliani <ppoliani@gmail.com>
2022-10-31 21:37:49 +0200

    chore(crypto): tmp changes

--- src/ticketland-crypto/src/symetric/hmac.rs
+++ src/ticketland-crypto/src/symetric/hmac.rs
@@ -1,11 +1,11 @@
 use eyre::Result;
 use sha2::Sha256;
-use hmac::{Hmac, Mac};
-// use ring::{
-//   digest,
-//   hmac,
-//   rand
-// };
+// use hmac::{Hmac, Mac};
+use ring::{
+  digest,
+  hmac,
+  rand
+};
 
 pub fn sign_sha256(secret_key: &str, msg: &str) -> Result<String> {
   let key = base64::decode(secret_key)?;
@@ -15,13 +15,13 @@ pub fn sign_sha256(secret_key: &str, msg: &str) -> Result<String> {
   Ok(hex::encode(&tag.as_ref()))
 }
 
-pub fn sign_sha256_2(secret_key: &str, message: &str) -> Result<String> {
-  let key = base64::decode(secret_key)?;
-  let mut mac = Hmac::<Sha256>::new_from_slice(key.as_ref()).unwrap();
+// pub fn sign_sha256_2(secret_key: &str, message: &str) -> Result<String> {
+//   let key = base64::decode(secret_key)?;
+//   let mut mac = Hmac::<Sha256>::new_from_slice(key.as_ref()).unwrap();
 
-  mac.update(format!("{}", message).as_bytes());
-  Ok(hex::encode(&mac.finalize().into_bytes()[..]))
-}
+//   mac.update(format!("{}", message).as_bytes());
+//   Ok(hex::encode(&mac.finalize().into_bytes()[..]))
+// }
 
 
 pub fn create_key() -> Result<String>{

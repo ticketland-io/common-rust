3d8085d921984777ec0ddb432d0451f91a084711
ppoliani <ppoliani@gmail.com>
2022-10-31 18:16:09 +0200

    feat(crypto): implement verify

--- src/crypto/src/ec/ed25519.rs
+++ src/crypto/src/ec/ed25519.rs
@@ -1,8 +1,18 @@
 use rand::rngs::OsRng;
-use ed25519_dalek::Keypair;
-use ed25519_dalek::Signature;
+use eyre::Result;
+use ed25519_dalek::{
+  Keypair,
+  PublicKey,
+  Signature,
+  Verifier,
+};
 
 pub fn create_keypair() -> Keypair {
   let mut csprng = OsRng{};
   Keypair::generate(&mut csprng)
+}
+
+pub fn verify(msg: &[u8], pub_key: &[u8], signature: Signature) -> Result<()> {
+  let public_key = PublicKey::from_bytes(pub_key)?;
+  Ok(public_key.verify(msg, &signature)?)
 }

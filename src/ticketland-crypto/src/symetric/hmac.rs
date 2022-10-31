2a8164e5889db0d4e429325169cbffddd039a43c
ppoliani <ppoliani@gmail.com>
2022-10-31 21:15:57 +0200

    feat(crypto): implement hmac create key

--- src/ticketland-crypto/src/symetric/hmac.rs
+++ src/ticketland-crypto/src/symetric/hmac.rs
@@ -1,11 +1,21 @@
 use eyre::Result;
-use sha2::Sha256;
-use hmac::{Hmac, Mac};
+use ring::{
+  digest,
+  hmac,
+  rand
+};
 
-pub fn sign_sha256(secret_key: &str, message: &str) -> Result<String> {
+pub fn sign_sha256(secret_key: &str, msg: &str) -> Result<String> {
   let key = base64::decode(secret_key)?;
-  let mut mac = Hmac::<Sha256>::new_from_slice(key.as_ref()).unwrap();
+  let s_key = hmac::Key::new(hmac::HMAC_SHA256, key.as_ref());
+  let tag = hmac::sign(&s_key, msg.as_bytes());
 
-  mac.update(format!("{}", message).as_bytes());
-  Ok(hex::encode(&mac.finalize().into_bytes()[..]))
+  Ok(hex::encode(&tag.as_ref()))
+}
+
+pub fn create_key() -> Result<String>{
+  let rng = rand::SystemRandom::new();
+  let key_value: [u8; digest::SHA256_OUTPUT_LEN] = rand::generate(&rng)?.expose();
+
+  Ok(hex::encode(&key_value))
 }

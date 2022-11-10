040f20ed64fadb3869de11524dfc58c14b8e8595
ppoliani <ppoliani@gmail.com>
2022-11-10 17:57:15 +0200

    feat: imlement read_canva_designs

--- src/ticketland-data/src/repositories/canva_design.rs
+++ src/ticketland-data/src/repositories/canva_design.rs
@@ -5,20 +5,43 @@ use crate::{
   connection::PostgresConnection,
   models::{
     canva_design::CanvaDesign,
+    canva_account::CanvaAccount,
   },
-  schema::canva_designs::dsl::*,
+  schema::{
+    canva_designs::dsl::{
+      self as canva_designs_dsl,
+      canva_designs,
+    },
+    canva_accounts::dsl::{
+      self as canva_accounts_dsl,
+      canva_accounts,
+    },
+  },
 };
 
 impl PostgresConnection {
   pub async fn upsert_ticket_design(&mut self, design: CanvaDesign) -> Result<()> {
     diesel::insert_into(canva_designs)
     .values(&design)
-    .on_conflict(design_id)
+    .on_conflict(canva_designs_dsl::design_id)
     .do_update()
     .set(&design)
     .execute(self.borrow_mut())
     .await?;
     
     Ok(())
+  }
+
+  pub async fn read_canva_designs(&mut self, account_id: String) -> Result<Vec<CanvaDesign>> {
+    Ok(
+      canva_accounts
+      .filter(canva_accounts_dsl::account_id.eq(account_id))
+      .inner_join(canva_designs.on(canva_designs_dsl::canva_uid.eq(canva_accounts_dsl::canva_uid)))
+      .load::<(CanvaAccount, CanvaDesign)>(self.borrow_mut())
+      .await?
+      .into_iter()
+      .map(|r| r.1)
+      .collect()
+    )
   }
 }

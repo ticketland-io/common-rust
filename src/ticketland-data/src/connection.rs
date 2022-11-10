e23b04dc3a377060264f3142dda6f2b06c68f2ee
ppoliani <ppoliani@gmail.com>
2022-11-10 12:52:59 +0200

    feat: add account model

--- src/ticketland-data/src/connection.rs
+++ src/ticketland-data/src/connection.rs
@@ -1,24 +1,17 @@
-use std::ops::{Deref, DerefMut};
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
 pub struct PostgresConnection(AsyncPgConnection);
 
-impl Deref for PostgresConnection {
-  type Target = AsyncPgConnection;
-
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
-}
-
-impl DerefMut for PostgresConnection {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.0
-  }
-}
-
 impl PostgresConnection {
   pub async fn new(db_uri: &str) -> Self {
     Self(AsyncPgConnection::establish(&db_uri).await.unwrap())
+  }
+
+  pub fn borrow(&mut self) -> &AsyncPgConnection {
+    &self.0
+  }
+
+  pub fn borrow_mut(&mut self) -> &mut AsyncPgConnection {
+    &mut self.0
   }
 }
--- src/ticketland-data/src/repositories/account.rs
+++ src/ticketland-data/src/repositories/account.rs
@@ -1,28 +1,31 @@
 use diesel::prelude::*;
 use eyre::Result;
-use diesel_async::{RunQueryDsl, AsyncPgConnection};
+use diesel_async::RunQueryDsl;
 use crate::{
+  connection::PostgresConnection,
   models::account::Account,
   schema::accounts::dsl::*,
 };
 
-pub async fn upsert_account(conn: &mut AsyncPgConnection, account: Account) -> Result<()> {
-  diesel::insert_into(accounts)
-  .values(&account)
-  .on_conflict(name)
-  .do_update()
-  .set(&account)
-  .execute(conn)
-  .await?;
+impl PostgresConnection {
+  pub async fn upsert_account(&mut self, account: Account) -> Result<()> {
+    diesel::insert_into(accounts)
+    .values(&account)
+    .on_conflict(name)
+    .do_update()
+    .set(&account)
+    .execute(self.borrow_mut())
+    .await?;
+    
+    Ok(())
+  }
   
-  Ok(())
-}
-
-pub async fn read_account_by_id(conn: &mut AsyncPgConnection, user_id: String) -> Result<Account> {
-  Ok(
-    accounts
-    .filter(uid.eq(user_id))
-    .first(conn)
-    .await?
-  )
+  pub async fn read_account_by_id(&mut self, user_id: String) -> Result<Account> {
+    Ok(
+      accounts
+      .filter(uid.eq(user_id))
+      .first(self.borrow_mut())
+      .await?
+    )
+  }
 }

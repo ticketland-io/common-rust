285ebb91815d5b9ee6aa32fe28f6e3531aaa7003
ppoliani <ppoliani@gmail.com>
2023-03-18 19:00:03 +0200

    chore: create key from string

--- src/ticketland-crypto/src/symetric/aes.rs
+++ src/ticketland-crypto/src/symetric/aes.rs
@@ -1,7 +1,7 @@
 use eyre::{Result, ErrReport};
 use aes_gcm_siv::{
   aead::{Aead, KeyInit},
-  Aes256GcmSiv, Nonce,
+  Aes256GcmSiv, Nonce, Key
 };
 
 /// Encrypts the given message
@@ -12,7 +12,8 @@ use aes_gcm_siv::{
 /// * `nonce`: unique nonce per message
 /// * `plaintext`: the text to encrypt
 pub fn encrypt(key: &[u8], nonce: &[u8], plaintext: &[u8]) -> Result<String> {
-  let cipher = Aes256GcmSiv::new(key.into());
+  let key = Key::<Aes256GcmSiv>::from_slice(key);
+  let cipher = Aes256GcmSiv::new(key);
   let nonce = Nonce::from_slice(nonce);
   let ciphertext = cipher.encrypt(nonce, plaintext).map_err(|error| ErrReport::msg(error.to_string()))?;
 
@@ -27,7 +28,8 @@ pub fn encrypt(key: &[u8], nonce: &[u8], plaintext: &[u8]) -> Result<String> {
 /// * `nonce`: unique nonce per message
 /// * `ciphertext`: the ciphertext to decrypt
 pub fn decrypt(key: &[u8], nonce: &[u8], ciphertext: &[u8]) -> Result<String> {
-  let cipher = Aes256GcmSiv::new(key.into());
+  let key = Key::<Aes256GcmSiv>::from_slice(key);
+  let cipher = Aes256GcmSiv::new(key);
   let nonce = Nonce::from_slice(nonce);
   let plaintext = cipher.decrypt(nonce, ciphertext).map_err(|error| ErrReport::msg(error.to_string()))?;
   let plaintext = hex::decode(plaintext)?;
